6f081fe6ccc6683ff9a89763dc3299d8c448889f
Ubuntu <ubuntu@ip-172-31-27-126.us-east-2.compute.internal>
2024-07-01 06:36:42 +0000

    s

--- client/src/api/RestaurantFinder.js
+++ client/src/api/RestaurantFinder.js
@@ -1,17 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
+const baseURL = process.env.NODE_ENV === 'production'
+    ? 'https://ratemystudyspot.com/api/v1/restaurants'
+    : 'http://localhost:3000/api/v1/restaurants';
 
-// decide production mode or dev mod 
-// NODE_ENV -> set production or a dev
-
-//if we are in production, we use the base ulr of the /api/v1/restaurants
-//else base url will be whatever before 
-
-
-const baseURL = process.env.NODE_ENV === "production"? "api/v1/restaurants" :"http://localhost:3000/api/v1/restaurants"
-
-
-
-export default axios.create({
+const instance = axios.create({
     baseURL,
 });
+export default instance;
